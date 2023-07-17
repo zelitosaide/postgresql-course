444ec29676fc21ce6f1f61541fd7e8f6ed2e925f
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-18 00:05:00 +0200

    stablish connection

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Ola");
+import pg from "pg";
+
+const client = new pg.Client({
+  port: 5432,
+  database: "mydb",
+  user: "zas"
+});
+
+client.connect(function(error) {
+  if (error) {
+    console.log(error, "zas");
+  } else {
+    console.log("Connected!");
+  }
+});
